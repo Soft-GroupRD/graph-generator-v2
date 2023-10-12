160cb628484b11ed1c9e5945c619bfbac4a03911
brstevenn <brayangaitan81@gmail.com>
2023-10-11 22:52:29 -0500

    change port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const igwinTemplate = require('./routes/templates/igwin')
 
 const app = express();
-const port = 8800;
+const port = 5500;
 
 const swaggerOptions = {
   swaggerDefinition: {
